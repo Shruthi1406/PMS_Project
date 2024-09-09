1f8da2487d5e9e38e2e80ff411adaf232d1e8a9b
SanthirajuMerla <SanthirajuMerla@DESKTOP-SE2TGKQ>
2024-09-09 15:15:02 +0530

    Modified configuration

--- PMS.Domain/obj/Debug/net8.0/PMS.Domain.AssemblyInfo.cs
+++ PMS.Domain/obj/Debug/net8.0/PMS.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PMS.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4124f7d81c94768a6a05b71e4461ebf4d491f5de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e65efb31f4029571fb89205c91fefa0e07d748c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PMS.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PMS.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
